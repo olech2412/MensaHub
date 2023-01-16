4b913d2725efaa8866b21cd2addd9ccf5a2bc2ac
Ole Einar Christoph <olechristoph2412@gmail.com>
2023-01-16 11:06:24 +0100

    first Controller for meals

---
+++ src/main/java/com/example/demo/Controller/MealController.java
@@ -0,0 +1,25 @@
+package com.example.demo.Controller;
+
+import com.example.demo.JPA.Meal;
+import com.example.demo.JPA.repository.MealRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class MealController {
+
+    private final MealRepository mealRepository;
+
+    public MealController(MealRepository mealRepository) {
+        this.mealRepository = mealRepository;
+    }
+
+    @GetMapping("/meals")
+    public Iterable<Meal> getMeals() {
+        log.info("Meals were requested");
+        return mealRepository.findAll();
+    }
+
+}
