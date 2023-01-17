14123f2be43d4fa1f946f96d5d86314abf9de329
Ole Einar Christoph <olechristoph2412@gmail.com>
2023-01-17 19:37:50 +0100

    übernahme der JPA Änderungen aus EssensGetter2.0

--- src/main/java/com/example/demo/JPA/Meal.java
+++ src/main/java/com/example/demo/JPA/Meal.java
@@ -1,5 +1,6 @@
 package com.example.demo.JPA;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,34 +10,36 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode
 @Table(name = "meals")
 public class Meal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Exclude
     @Column(name = "id", nullable = false)
     private Long id;
     private String name;
     private String description;
     private String price;
     private String category;
-    private LocalDate creationDate;
+    private LocalDate servingDate;
 
     private Integer responseCode;
 
     public Meal() {
     }
 
-    public Meal(String name, String description, String price, String category, LocalDate creationDate) {
+    public Meal(String name, String description, String price, String category, LocalDate servingDate) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
-        this.creationDate = creationDate;
+        this.servingDate = servingDate;
     }
 
     @Override
     public String toString() {
-        return "Meal: " + "name=" + name + ", description=" + description + ", price=" + price + ", category=" + category + ", creationDate=" + creationDate;
+        return "Meal: " + "name=" + name + ", description=" + description + ", price=" + price + ", category=" + category + ", servingDate=" + servingDate;
     }
 }
