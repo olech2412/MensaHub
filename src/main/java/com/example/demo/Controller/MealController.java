95731cb3b06a5971d5eab39e3f50110217abd3de
whosFritz <97552289+whosFritz@users.noreply.github.com>
2023-01-21 17:21:55 +0100

    added "@CrossOriging" lines 23 and 30

--- src/main/java/com/example/demo/Controller/MealController.java
+++ src/main/java/com/example/demo/Controller/MealController.java
@@ -20,12 +20,14 @@ public class MealController {
         this.mealRepository = mealRepository;
     }
 
+    @CrossOrigin
     @GetMapping("/mealsForFritz")
     public Iterable<Meal> getMeals() {
         log.debug("Meals were requested");
         return mealRepository.findAllByServingDateGreaterThanEqual(LocalDate.now().minusDays(2));
     }
 
+    @CrossOrigin
     @PostMapping("/mealsFromFritz")
     @Transactional
     public void saveMeal(@RequestBody Meal receivedMeal) {
