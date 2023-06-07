f21e6bde0302d15586c1ae306f4e130e6587835d
Fritz <97552289+whosFritz@users.noreply.github.com>
2023-06-07 15:48:40 +0200

    Update ControllerSchoenauerStr.java

--- src/main/java/de/mensahub/gateway/Controller/ControllerSchoenauerStr.java
+++ src/main/java/de/mensahub/gateway/Controller/ControllerSchoenauerStr.java
@@ -14,6 +14,7 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 
+@CrossOrigin(origins = "https://mensi-mates.whosfritz.de")
 @RestController
 @Log4j2
 @RequestMapping("/mensa_schoenauerstr")
