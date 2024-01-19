81b6f01654879d466080a74a1820d7f7e890981a
olech2412 <olechristoph2412@gmail.com>
2024-01-19 20:33:06 +0100

    - removed /register endpoint - changed workflow for register api user

--- MensaHub-Gateway/src/main/java/de/olech2412/mensahub/gateway/controller/authentication/AuthenticationController.java
+++ MensaHub-Gateway/src/main/java/de/olech2412/mensahub/gateway/controller/authentication/AuthenticationController.java
@@ -4,18 +4,15 @@ import de.olech2412.mensahub.gateway.jpa.repository.API_UserRepository;
 import de.olech2412.mensahub.gateway.requests.LoginRequest;
 import de.olech2412.mensahub.gateway.security.config.JwtService;
 import de.olech2412.mensahub.models.authentification.API_User;
-import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,33 +22,14 @@ public class AuthenticationController {
 
     private final API_UserRepository apiUserRepository;
 
-    private final PasswordEncoder passwordEncoder;
-
     private final AuthenticationManager authenticationManager;
 
     private final JwtService jwtTokenProvider;
 
-    public AuthenticationController(API_UserRepository apiUserRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtTokenProvider) {
+    public AuthenticationController(API_UserRepository apiUserRepository, AuthenticationManager authenticationManager, JwtService jwtTokenProvider) {
         this.apiUserRepository = apiUserRepository;
-        this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<API_User> register(@Valid @RequestBody API_User sentApiUser) {
-        Optional<API_User> userOptional = apiUserRepository.findAPI_UserByApiUsername(sentApiUser.getApiUsername());
-        if (userOptional.isPresent()) {
-            log.warn("User tried to register an existing user");
-            return ResponseEntity.badRequest().build();
-        }
-
-        sentApiUser.setPassword(passwordEncoder.encode(sentApiUser.getPassword()));
-
-        apiUserRepository.save(sentApiUser);
-
-        return ResponseEntity.ok(sentApiUser);
-
     }
 
     @PostMapping("/login")
