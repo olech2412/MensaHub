7f3d6e8ca1c31da8417b0b11b8b416528e0c3f72
olech2412 <olechristoph2412@gmail.com>
2024-07-03 21:38:35 +0200

    hotfix

--- MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/views/main/MailSettingsView.java
+++ MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/views/main/MailSettingsView.java
@@ -207,7 +207,7 @@ public class MailSettingsView extends Composite implements BeforeEnterObserver {
         mailUserRepository.delete(activatedUser);
         deactivationCodeRepository.delete(activatedUser.getDeactivationCode());
 
-        if (!activatedUser.getActivationCode().getCode().equals("387UxMzB12")) { // if user is not activated
+        if (!activatedUser.getActivationCode().getCode().equals("bereits aktiviert")) { // if user is not activated
             activationCodeRepository.delete(activatedUser.getActivationCode());
         }
 
