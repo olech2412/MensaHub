4a27ed5179d1bfbf3ca6b29bad3a2ade7be76787
olech2412 <olechristoph2412@gmail.com>
2024-08-05 23:10:07 +0200

    fix stupid shit

---
+++ MensaHub-DataDispatcher/src/main/java/de/olech2412/mensahub/datadispatcher/jpa/repository/RatingRepository.java
@@ -0,0 +1,16 @@
+package de.olech2412.mensahub.datadispatcher.jpa.repository;
+
+import de.olech2412.mensahub.models.Rating;
+import de.olech2412.mensahub.models.authentification.MailUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    List<Rating> findAllByMailUserAndMealName(MailUser mailUser, String mealName);
+
+    Optional<Rating> findByMealId(Long mealId);
+
+}
