0eba67135fe715279f5b9c9d0cf1731882d505f2
olech2412 <olechristoph2412@gmail.com>
2024-08-05 23:18:50 +0200

    neue Datei:     MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/gui/components/vaadin/notifications/types/InfoWithAnchorNotification.java

---
+++ MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/gui/components/vaadin/notifications/types/InfoWithAnchorNotification.java
@@ -0,0 +1,42 @@
+package de.olech2412.mensahub.junction.gui.components.vaadin.notifications.types;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.function.SerializableConsumer;
+import com.vaadin.flow.router.RouterLink;
+
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.page.Page;
+import lombok.Getter;
+
+@JavaScript("frontend://local-storage.js")
+@Getter
+public class InfoWithAnchorNotification extends MensaHubNotification {
+
+    Button closeButton;
+
+    public InfoWithAnchorNotification(String message, String anchorText, Class<? extends Component> navigationTarget) {
+        super(message);
+
+        RouterLink link = new RouterLink(anchorText, navigationTarget);
+        link.addClassName("shimmer");
+        Div text = new Div(new Text(message),link);
+
+        closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.setAriaLabel("Close");
+
+        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        add(layout);
+        setDuration(-1);
+        setPosition(Position.MIDDLE);
+    }
+}
