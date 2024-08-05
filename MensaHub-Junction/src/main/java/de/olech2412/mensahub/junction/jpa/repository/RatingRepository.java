5f03b4cb15bac036a09ae7e01710c4f6d5300a59
olech2412 <olechristoph2412@gmail.com>
2024-08-05 23:16:26 +0200

    neue Datei:     MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/jpa/repository/RatingRepository.java

---
+++ MensaHub-Junction/src/main/java/de/olech2412/mensahub/junction/jpa/repository/RatingRepository.java
@@ -0,0 +1,13 @@
+package de.olech2412.mensahub.junction.jpa.repository;
+
+import de.olech2412.mensahub.models.Rating;
+import de.olech2412.mensahub.models.authentification.MailUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    List<Rating> findAllByMailUserAndMealName(MailUser mailUser, String mealName);
+
+}
